b6d8b0bac75c4c30d4e1d5f576efd9cad96707f2
MohmmedHasan-99 <mo7ammed.mah@gmail.com>
2021-07-16 15:36:14 +0300

    make the function of add task button relates #17

--- script/main.js
+++ script/main.js
@@ -18,3 +18,31 @@ window.onclick = function (event) {
     formBlock.style.display = "none";
   }
 };
+
+add.onclick = function () {
+  let task_name = document.getElementById("task_name").value;
+  let task_due = document.getElementById("due_time").value;
+  let task_details = document.getElementById("task_details").value;
+
+  let data = {
+    //create object for each task data
+    name: task_name,
+    due_time: task_due,
+    details: task_details,
+    isDone: false,
+  };
+
+  let temp; //declare temp variable
+
+  if (localStorage.getItem("tasks") === null) 
+    temp = [];    //if there is no tasks array (initially), create an array for tasks
+
+  else  // if there is already tasks array in the local storage bring it temporary to temp var
+    temp = JSON.parse(localStorage.getItem("tasks"));
+       
+
+  temp.push(data); //appending the new task object to task array
+
+  localStorage.setItem("tasks", JSON.stringify(temp));
+  //replace tasks array in the local stirge with the new appended version
+};
